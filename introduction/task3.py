17effd12d1803d213a7238c4e1effb6e49df7aa1
deska13 <dkuzmin@axon.expert>
2024-08-14 16:10:14 +0500

    Completed introduction

---
+++ introduction/task3.py
@@ -0,0 +1,6 @@
+# 3rd program
+a = 2 * 2 + 2
+print(a)
+b = 2 * (2 + 2)
+print(b)
+print(a == b)
---
+++ introduction/task4.py
@@ -0,0 +1,5 @@
+# 4th program
+input_str = '123.456'
+input_float = float(input_str)
+result = int(input_float * 10 % 10)
+print(result)
