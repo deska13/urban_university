33bfe25cc82ce96eacdf3edbccaf5e177a7e8a11
deska13 <server@DESKTOP-IFSJV3J>
2024-08-17 12:52:37 +0500

    .

---
+++ basic_struct/variables/main.py
@@ -0,0 +1,5 @@
+count_completed_homework = 12
+hours_homework = 1.5
+course_name = "Python"
+avg_task_hours = hours_homework / count_completed_homework
+print("Курс:", course_name, ", всего задач:", count_completed_homework, ", затрачено часов:", hours_homework , ", среднее время выполнения", avg_task_hours, "часа.")
