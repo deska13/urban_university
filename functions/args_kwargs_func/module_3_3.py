1c6967a299694343af08689c8102f99aa2ea3d7d
deska13kuzmin <deskakuzmin1999@gmail.com>
2024-08-25 12:26:56 +0500

    Module 3 3

---
+++ functions/args_kwargs_func/module_3_3.py
@@ -0,0 +1,14 @@
+def print_params(a=1, b="строка", c=True):
+    print(a, b, c)
+
+
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+values_list = [3, "str", False]
+values_dict = {"a": 3, "b": "str", "c": False}
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [5, "temp"]
+print_params(*values_list_2, 42)  # type: ignore[call-arg]
